313943606c8c0413f799c1918aabf645df248fb1
帅维灿 <shuaiweican@douyu.tv>
2018-06-02 22:00:03 +0800

    更新

--- android/aapt_print.cmd
+++
@@ -1,4 +0,0 @@
-d:
-cd D:\doc\my\android\aapt_test\TestApp
-aapt.exe p -M app/src\main/AndroidManifest.xml -v -S app\src\main\res -I %ANDROID_HOME%\platforms\android-27\android.jar -F tmp.zip
-pause
