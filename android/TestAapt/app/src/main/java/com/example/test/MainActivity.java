0f6b9e6b0c4e7eeecb61f1c18072a8d4b5dce2e3
jinshiyi11 <49662990@qq.com>
2018-07-29 21:05:14 +0800

    UPDATE

--- android/TestAapt/app/src/main/java/com/example/test/MainActivity.java
+++ android/TestAapt/app/src/main/java/com/example/test/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.test;
 
+import android.app.Activity;
 import android.graphics.drawable.Drawable;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
